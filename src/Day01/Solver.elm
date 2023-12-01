122a3c3981c4d87de7117e457dcb01fbea57142c
James Gary <jgary@triumphpay.com>
2023-11-30 21:28:07 -0800

    Day01b

--- src/Day01/Solver.elm
+++ src/Day01/Solver.elm
@@ -1,11 +1,144 @@
 module Day01.Solver exposing (..)
 
 import Day01.Input exposing (input)
+import Dict exposing (Dict)
+import List.Extra
+
+
+numStringDict =
+    [ ( "one", 1 )
+    , ( "two", 2 )
+    , ( "three", 3 )
+    , ( "four", 4 )
+    , ( "five", 5 )
+    , ( "six", 6 )
+    , ( "seven", 7 )
+    , ( "eight", 8 )
+    , ( "nine", 9 )
+    ]
+        |> Dict.fromList
 
 
 solve : String
 solve =
-    -- test: sum all lines
+    input
+        |> String.trim
+        |> String.lines
+        |> List.map
+            (\line ->
+                let
+                    firstNumStringWithIndex : Maybe ( Int, Int )
+                    firstNumStringWithIndex =
+                        numStringDict
+                            |> Dict.toList
+                            |> List.filterMap
+                                (\( numStr, num ) ->
+                                    line
+                                        |> String.indexes numStr
+                                        |> List.head
+                                        |> Maybe.map (\index -> ( num, index ))
+                                )
+                            |> List.Extra.minimumBy (\( num, index ) -> index)
+                            |> Debug.log "firstNumString"
+
+                    lastNumStringWithIndex : Maybe ( Int, Int )
+                    lastNumStringWithIndex =
+                        numStringDict
+                            |> Dict.toList
+                            |> List.filterMap
+                                (\( numStr, num ) ->
+                                    line
+                                        |> String.indexes numStr
+                                        |> List.reverse
+                                        |> List.head
+                                        |> Maybe.map (\index -> ( num, index ))
+                                )
+                            |> List.Extra.maximumBy (\( num, index ) -> index)
+
+                    chars : List String
+                    chars =
+                        line
+                            |> String.toList
+                            |> List.map String.fromChar
+
+                    firstNumWithIndex : Maybe ( Int, Int )
+                    firstNumWithIndex =
+                        chars
+                            |> List.indexedMap (\i char -> ( char, i ))
+                            |> List.filterMap
+                                (\( char, i ) ->
+                                    char
+                                        |> String.toInt
+                                        |> Maybe.map (\int -> ( int, i ))
+                                )
+                            |> List.head
+
+                    firstNum =
+                        (case ( firstNumStringWithIndex, firstNumWithIndex ) of
+                            ( Just ( num1, i1 ), Just ( num2, i2 ) ) ->
+                                if i1 < i2 then
+                                    num1
+
+                                else
+                                    num2
+
+                            ( Just ( num, i ), Nothing ) ->
+                                num
+
+                            ( Nothing, Just ( num, i ) ) ->
+                                num
+
+                            ( Nothing, Nothing ) ->
+                                0
+                                    |> Debug.log "???"
+                        )
+                            |> String.fromInt
+
+                    lastNumWithIndex : Maybe ( Int, Int )
+                    lastNumWithIndex =
+                        chars
+                            |> List.indexedMap (\i char -> ( char, i ))
+                            |> List.filterMap
+                                (\( char, i ) ->
+                                    char
+                                        |> String.toInt
+                                        |> Maybe.map (\int -> ( int, i ))
+                                )
+                            |> List.reverse
+                            |> List.head
+
+                    lastNum =
+                        (case ( lastNumStringWithIndex, lastNumWithIndex ) of
+                            ( Just ( num1, i1 ), Just ( num2, i2 ) ) ->
+                                if i1 > i2 then
+                                    num1
+
+                                else
+                                    num2
+
+                            ( Just ( num, i ), Nothing ) ->
+                                num
+
+                            ( Nothing, Just ( num, i ) ) ->
+                                num
+
+                            ( Nothing, Nothing ) ->
+                                0
+                                    |> Debug.log "???!!!"
+                        )
+                            |> String.fromInt
+                in
+                (firstNum ++ lastNum)
+                    |> Debug.log "OK"
+                    |> String.toInt
+                    |> Maybe.withDefault 0
+            )
+        |> List.sum
+        |> String.fromInt
+
+
+solve_ : String
+solve_ =
     input
         |> String.trim
         |> String.lines
