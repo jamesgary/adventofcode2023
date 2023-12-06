79221cbf893ffc5dcc659053ebf03190c1c2cf6f
James Gary <jgary@triumphpay.com>
2023-12-06 00:35:09 -0800

    Start of Day05b ;_;

--- src/Day05/Solver.elm
+++ src/Day05/Solver.elm
@@ -16,58 +16,39 @@ type alias Route =
 solve : String
 solve =
     let
-        ( seeds, maps ) =
-            inputToSeedsAndMaps
+        ( seedRanges, maps ) =
+            inputToSeedRangesAndMaps
 
         lowestLocation =
-            seeds
-                |> List.map (seedToLocation maps)
-                |> List.sort
-                |> List.head
-                |> Maybe.withDefault -1
+            maps
+                |> List.reverse
+                |> findLowestLocation seedRanges
     in
     lowestLocation
         |> String.fromInt
 
 
-seedToLocation : List (List Route) -> Int -> Int
-seedToLocation maps seed =
-    maps
-        |> List.foldl
-            (\routes source ->
-                let
-                    route =
-                        routes
-                            -- already in descending order by sourceRangeStart
-                            |> List.Extra.find
-                                (\r ->
-                                    r.sourceRangeStart <= source && source <= r.sourceRangeStart + r.rangeLength
-                                )
+findLowestLocation : List ( Int, Int ) -> List (List Route) -> Int
+findLowestLocation seedRanges maps =
+    case maps of
+        [] ->
+            Debug.todo "empty list, should never get here!"
 
-                    dest =
-                        case route of
-                            Just r ->
-                                (source - r.sourceRangeStart)
-                                    + r.destRangeStart
-
-                            Nothing ->
-                                source
-                in
-                dest
-            )
-            seed
+        finalMap ->
+            -- try to find seed for lowest route
+            Debug.todo "wutttttt"
 
 
-inputToSeedsAndMaps : ( List Int, List (List Route) )
-inputToSeedsAndMaps =
+inputToSeedRangesAndMaps : ( List ( Int, Int ), List (List Route) )
+inputToSeedRangesAndMaps =
     case input |> String.trim |> String.split "\n\n" of
         [] ->
             ( [], [] )
 
         seedsStr :: rest ->
             let
-                seeds : List Int
-                seeds =
+                seedRanges : List ( Int, Int )
+                seedRanges =
                     seedsStr
                         |> String.split ": "
                         |> List.drop 1
@@ -75,39 +56,126 @@ inputToSeedsAndMaps =
                         |> Maybe.withDefault ""
                         |> String.split " "
                         |> List.filterMap String.toInt
+                        |> List.Extra.groupsOf 2
+                        |> List.map
+                            (\group ->
+                                case group of
+                                    [ a, b ] ->
+                                        ( a, b )
 
-                -- challenge b
-                -- |> List.Extra.groupsOf 2
-                -- |> List.map
-                --     (\group ->
-                --         case group of
-                --             [ a, b ] ->
-                --                 List.range 0 (b - 1)
-                --                     |> List.map ((+) a)
-                --             _ ->
-                --                 Debug.todo "odd nums"
-                --     )
-                -- |> List.concat
+                                    _ ->
+                                        Debug.todo "odd nums"
+                            )
+
                 maps : List (List Route)
                 maps =
                     rest
                         |> List.map
                             (\mapStr ->
-                                mapStr
-                                    |> String.lines
-                                    |> List.drop 1
-                                    |> List.map
-                                        (\routeStr ->
-                                            case routeStr |> String.split " " |> List.filterMap String.toInt of
-                                                [ d, s, r ] ->
-                                                    Route s d r
+                                let
+                                    routes =
+                                        mapStr
+                                            |> String.lines
+                                            |> List.drop 1
+                                            |> List.map
+                                                (\routeStr ->
+                                                    case routeStr |> String.split " " |> List.filterMap String.toInt of
+                                                        [ d, s, r ] ->
+                                                            Route s d r
 
-                                                _ ->
-                                                    Debug.todo ("bad routeStr: " ++ routeStr)
-                                        )
-                                    -- sort them all by source range start descending
-                                    |> List.sortBy .sourceRangeStart
-                                    |> List.reverse
+                                                        _ ->
+                                                            Debug.todo ("bad routeStr: " ++ routeStr)
+                                                )
+                                            |> List.sortBy .sourceRangeStart
+
+                                    zeroRoute : List Route
+                                    zeroRoute =
+                                        case List.head routes of
+                                            Just head ->
+                                                [ { sourceRangeStart = 0
+                                                  , destRangeStart = 0
+                                                  , rangeLength = head.sourceRangeStart
+                                                  }
+                                                ]
+
+                                            Nothing ->
+                                                []
+                                in
+                                zeroRoute ++ routes
                             )
             in
-            ( seeds, maps )
+            ( seedRanges, maps )
+
+
+
+{-
+   seedToLocation_ : List (List Route) -> Int -> Int
+   seedToLocation_ maps seed =
+       maps
+           |> List.foldl
+               (\routes source ->
+                   let
+                       route =
+                           routes
+                               -- already in descending order by sourceRangeStart
+                               |> List.Extra.find
+                                   (\r ->
+                                       r.sourceRangeStart <= source && source <= r.sourceRangeStart + r.rangeLength
+                                   )
+
+                       dest =
+                           case route of
+                               Just r ->
+                                   (source - r.sourceRangeStart)
+                                       + r.destRangeStart
+
+                               Nothing ->
+                                   source
+                   in
+                   dest
+               )
+               seed
+
+
+   inputToSeedsAndMaps_ : ( List Int, List (List Route) )
+   inputToSeedsAndMaps_ =
+       case input |> String.trim |> String.split "\n\n" of
+           [] ->
+               ( [], [] )
+
+           seedsStr :: rest ->
+               let
+                   seeds : List Int
+                   seeds =
+                       seedsStr
+                           |> String.split ": "
+                           |> List.drop 1
+                           |> List.head
+                           |> Maybe.withDefault ""
+                           |> String.split " "
+                           |> List.filterMap String.toInt
+
+                   maps : List (List Route)
+                   maps =
+                       rest
+                           |> List.map
+                               (\mapStr ->
+                                   mapStr
+                                       |> String.lines
+                                       |> List.drop 1
+                                       |> List.map
+                                           (\routeStr ->
+                                               case routeStr |> String.split " " |> List.filterMap String.toInt of
+                                                   [ d, s, r ] ->
+                                                       Route s d r
+
+                                                   _ ->
+                                                       Debug.todo ("bad routeStr: " ++ routeStr)
+                                           )
+                                       -- sort them all by source range start descending
+                                       |> List.sortBy .sourceRangeStart
+                                       |> List.reverse
+                               )
+               in
+               ( seeds, maps )
+-}
