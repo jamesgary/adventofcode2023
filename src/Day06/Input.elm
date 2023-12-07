c2e366dec09219dbe60798e41dc7b66c592cce6a
James Gary <jgary@triumphpay.com>
2023-12-06 20:58:14 -0800

    Day06b (changed input)

--- src/Day06/Input.elm
+++ src/Day06/Input.elm
@@ -3,13 +3,13 @@ module Day06.Input exposing (..)
 
 input_ =
     """
-Time:      7  15   30
-Distance:  9  40  200
+Time:      71530
+Distance:  940200
 """
 
 
 input =
     """
-Time:        40     82     84     92
-Distance:   233   1011   1110   1487
+Time:        40828492
+Distance:   233101111101487
 """
