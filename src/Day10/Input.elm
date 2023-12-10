3bd6181e4b45d14378295abcdb5e8ca09d78f7cb
James Gary <jgary@triumphpay.com>
2023-12-10 01:10:08 -0800

    Day10b

--- src/Day10/Input.elm
+++ src/Day10/Input.elm
@@ -11,6 +11,50 @@ LJ.LJ
 """
 
 
+inputB =
+    """
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"""
+
+
+inputB2 =
+    """
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+"""
+
+
+inputB0 =
+    """
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+"""
+
+
 input =
     """
 |-F7|77FJ-J7..L.-7-7.F--7-J7-FJ7|.FFFF7F-|7|7.J.-|7--F|-LL---77FF--7-|-LF77F|7FF-JFF7FJ-|7F.F7FFL-J7FFL-J-7-FFF7JF|-L77|-F7.FL7F7-7.7.FLF-F7
--- src/Day10/Solver.elm
+++ src/Day10/Solver.elm
@@ -23,7 +23,7 @@ solve =
                 |> Maybe.withDefault badPos
     in
     parsedTiles
-        |> getStepsToFurthestPos startPos
+        |> getNumInnerTiles startPos
         |> Debug.toString
 
 
@@ -43,10 +43,10 @@ type Pipe
 
 allRelNeighborPoses : List Pos
 allRelNeighborPoses =
-    [ ( -1, 0 )
-    , ( 0, -1 )
-    , ( 0, 1 )
-    , ( 1, 0 )
+    [ ( -1, 0 ) -- W
+    , ( 0, -1 ) -- S
+    , ( 0, 1 ) -- N
+    , ( 1, 0 ) -- E
     ]
 
 
@@ -127,6 +127,186 @@ validPipesForRelNeighborPos =
         |> Dict.fromList
 
 
+type State
+    = Out
+    | In
+      -- on pipe states
+    | InIfUnder
+    | InIfOver
+
+
+getNumInnerTiles : Pos -> Dict Pos Pipe -> Int
+getNumInnerTiles startPos pipes =
+    let
+        ( nextPos, walkDir ) =
+            allRelNeighborPoses
+                |> List.Extra.findMap
+                    (\relNeighborPos ->
+                        let
+                            neighborPos =
+                                add startPos relNeighborPos
+
+                            neighborPipe =
+                                pipes
+                                    |> Dict.get neighborPos
+                                    |> Maybe.withDefault Start
+
+                            isValid =
+                                validPipesForRelNeighborPos
+                                    |> Dict.get relNeighborPos
+                                    |> Maybe.map (List.member neighborPipe)
+                                    |> Maybe.withDefault False
+                        in
+                        if isValid then
+                            Just ( neighborPos, relNeighborPos )
+
+                        else
+                            Nothing
+                    )
+                |> Maybe.withDefault ( badPos, badPos )
+
+        loop =
+            buildLoop pipes ( startPos, nextPos ) [ nextPos, startPos ]
+
+        loopSet =
+            Set.fromList loop
+
+        minX =
+            loop
+                |> List.map Tuple.first
+                |> List.minimum
+                |> Maybe.withDefault -1
+
+        minY =
+            loop
+                |> List.map Tuple.second
+                |> List.minimum
+                |> Maybe.withDefault -1
+
+        maxX =
+            loop
+                |> List.map Tuple.first
+                |> List.maximum
+                |> Maybe.withDefault -1
+
+        maxY =
+            loop
+                |> List.map Tuple.second
+                |> List.maximum
+                |> Maybe.withDefault -1
+
+        numInnerTiles =
+            List.range minY maxY
+                |> List.map
+                    (\y ->
+                        List.range minX maxX
+                            |> List.foldl
+                                (\x ( count, state ) ->
+                                    let
+                                        pos =
+                                            ( x, y )
+
+                                        newState =
+                                            if Set.member pos loopSet then
+                                                case Dict.get pos pipes of
+                                                    Just Vert ->
+                                                        case state of
+                                                            Out ->
+                                                                In
+
+                                                            _ ->
+                                                                Out
+
+                                                    Just Horz ->
+                                                        -- skip horizontal bars
+                                                        state
+
+                                                    Just NE ->
+                                                        case state of
+                                                            Out ->
+                                                                InIfOver
+
+                                                            In ->
+                                                                InIfUnder
+
+                                                            _ ->
+                                                                -- impossible if on path
+                                                                state
+                                                                    |> Debug.log (Debug.toString pos)
+
+                                                    Just NW ->
+                                                        -- coming from west
+                                                        case state of
+                                                            InIfUnder ->
+                                                                In
+
+                                                            InIfOver ->
+                                                                Out
+
+                                                            _ ->
+                                                                -- impossible if not on path
+                                                                state
+                                                                    |> Debug.log (Debug.toString pos)
+
+                                                    Just SW ->
+                                                        -- coming from west
+                                                        case state of
+                                                            InIfUnder ->
+                                                                Out
+
+                                                            InIfOver ->
+                                                                In
+
+                                                            _ ->
+                                                                -- impossible if not on path
+                                                                state
+                                                                    |> Debug.log (Debug.toString pos)
+
+                                                    Just SE ->
+                                                        case state of
+                                                            Out ->
+                                                                InIfUnder
+
+                                                            In ->
+                                                                InIfOver
+
+                                                            _ ->
+                                                                -- impossible if on path
+                                                                state
+                                                                    |> Debug.log (Debug.toString pos)
+
+                                                    Just Start ->
+                                                        -- my input has -S-, so it's a non-changing pipe
+                                                        state
+
+                                                    Nothing ->
+                                                        state
+                                                            |> Debug.log ("hm " ++ Debug.toString pos)
+
+                                            else
+                                                state
+
+                                        isOnEmptyTile =
+                                            not <| Set.member pos loopSet
+                                    in
+                                    if newState == In && isOnEmptyTile then
+                                        let
+                                            _ =
+                                                Debug.log "in" pos
+                                        in
+                                        ( count + 1, newState )
+
+                                    else
+                                        ( count, newState )
+                                )
+                                ( 0, Out )
+                            |> Tuple.first
+                    )
+                |> List.sum
+    in
+    numInnerTiles
+
+
 getStepsToFurthestPos : Pos -> Dict Pos Pipe -> Int
 getStepsToFurthestPos startPos pipes =
     let
@@ -157,11 +337,8 @@ getStepsToFurthestPos startPos pipes =
                     )
                 |> Maybe.withDefault badPos
 
-        --|> Debug.log "nextPosForStart"
         loop =
             buildLoop pipes ( startPos, nextPos ) [ nextPos, startPos ]
-
-        --|> Debug.log "loop"
     in
     List.length loop // 2
 
@@ -172,7 +349,6 @@ buildLoop pipes ( prevPos, curPos ) visitedPipes =
         ( pos1, pos2 ) =
             connectedNeighborsForPos curPos pipes
 
-        --|> Debug.log ("cnfsp " ++ Debug.toString curPos)
         nextPos =
             if pos1 == prevPos then
                 pos2
